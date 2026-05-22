f9272ad838971a16126009880c6d9964931710a2
Chris <nerino1@gmail.com>
2026-05-21 20:56:12 -0400

    Update control.php

--- plugins/control.php
+++ plugins/control.php
@@ -1,8 +1,7 @@
 <?php
-// Service control endpoint for iDRAC Fan Controller plugin
-$rc_script = '/etc/rc.d/rc.idrac-fan-controller';
-$pid_file  = '/var/run/idrac-fan-controller.pid';
-$override_file = '/mnt/user/appdata/idrac-fan-controller/fan_override';
+$rc_script    = '/etc/rc.d/rc.idrac-fan-controller';
+$pid_file     = '/var/run/idrac-fan-controller.pid';
+$override_file= '/mnt/user/appdata/idrac-fan-controller/fan_override';
 
 header('Content-Type: application/json');
 
@@ -10,20 +9,20 @@ $action = $_POST['action'] ?? $_GET['action'] ?? '';
 
 switch ($action) {
   case 'start':
-    shell_exec("$rc_script start > /dev/null 2>&1 &");
-    sleep(1);
+    exec("nohup $rc_script start > /tmp/idrac-start.log 2>&1 &");
+    sleep(2);
     echo json_encode(['ok' => true, 'status' => getStatus()]);
     break;
 
   case 'stop':
-    shell_exec("$rc_script stop > /dev/null 2>&1");
+    exec("$rc_script stop > /tmp/idrac-stop.log 2>&1");
     sleep(1);
     echo json_encode(['ok' => true, 'status' => getStatus()]);
     break;
 
   case 'restart':
-    shell_exec("$rc_script restart > /dev/null 2>&1 &");
-    sleep(2);
+    exec("nohup $rc_script restart > /tmp/idrac-restart.log 2>&1 &");
+    sleep(3);
     echo json_encode(['ok' => true, 'status' => getStatus()]);
     break;
 
